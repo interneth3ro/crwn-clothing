cb1d54d50fbf67dafe2dc2a9c4b32535a5c1c89e
James McConnell <interneth3ro@gmail.com>
2022-09-20 09:17:10 -0500

    first commit

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,16 @@
+import { CartItemContainer, ItemDetails } from "./cart-item.styles";
+
+const CartItem = ({cartItem}) => {
+  const {name, quantity, price, imageUrl} = cartItem;
+  return (
+    <CartItemContainer>
+      <img src={imageUrl} alt={`{name}`} />
+      <ItemDetails>
+        <span>{name}</span>
+        <span>{quantity} x ${price}</span>
+      </ItemDetails>
+    </CartItemContainer>
+  )
+}
+
+export default CartItem;
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { Elements } from '@stripe/react-stripe-js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import App from './App';
+import './index.scss';
+import { stripePromise } from './utils/stripe/stripe.utils';
+
+import { persistor, store } from './store/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
